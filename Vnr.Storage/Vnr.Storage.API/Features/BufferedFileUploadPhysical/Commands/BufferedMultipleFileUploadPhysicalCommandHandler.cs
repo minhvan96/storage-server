1a399d65481e917aa656b105c460283bbf0be962
van.nguyen <minhvan.996@outlook.com>
2021-03-03 14:59:29 +0700

    update config

--- Vnr.Storage/Vnr.Storage.API/Features/BufferedFileUploadPhysical/Commands/BufferedMultipleFileUploadPhysicalCommandHandler.cs
+++ Vnr.Storage/Vnr.Storage.API/Features/BufferedFileUploadPhysical/Commands/BufferedMultipleFileUploadPhysicalCommandHandler.cs
@@ -19,12 +19,16 @@ namespace Vnr.Storage.API.Features.BufferedFileUploadPhysical.Commands
     public class BufferedMultipleFileUploadPhysicalCommandHandler : IRequestHandler<BufferedMultipleFileUploadPhysicalCommand, ResponseModel>
     {
         private readonly long _defaultFileSizeLimit;
-        private readonly string[] _permittedExtensions = { ".txt", ".pdf", ".docx" };
+        private readonly string[] _permittedExtensions;
         private readonly string _contentRootPath;
 
         public BufferedMultipleFileUploadPhysicalCommandHandler(IConfiguration configuration, IWebHostEnvironment env)
         {
             var fileSizeLimitConfiguration = configuration.GetSection(nameof(FileSizeLimitConfiguration)).Get<FileSizeLimitConfiguration>();
+            var bufferedFileUploadPhysicalPermittedExtensionsConfiguration = configuration
+                .GetSection(nameof(BufferedFileUploadPhysicalPermittedExtensionsConfiguration))
+                .Get<BufferedFileUploadPhysicalPermittedExtensionsConfiguration>();
+            _permittedExtensions = bufferedFileUploadPhysicalPermittedExtensionsConfiguration.MultipleFileUploadPermittedExtensions;
             _defaultFileSizeLimit = fileSizeLimitConfiguration.DefaultFileSizeLimit;
             _contentRootPath = env.ContentRootPath;
         }
