c7b65e816ba4af2136ec8e7e5471554216cd0375
van.nguyen <minhvan.996@outlook.com>
2021-03-03 16:04:47 +0700

    add decrypt multiple file

---
+++ Vnr.Storage/Vnr.Storage.API/Features/DecryptData/Commands/DecryptMultipleFileCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using Vnr.Storage.API.Infrastructure.Models;
+
+namespace Vnr.Storage.API.Features.DecryptData.Commands
+{
+    public class DecryptMultipleFileCommand : IRequest<FileContentResultModel>
+    {
+        public List<IFormFile> Files { get; set; }
+    }
+}
---
+++ Vnr.Storage/Vnr.Storage.API/Features/DecryptData/Commands/DecryptMultipleFileCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Vnr.Storage.API.Infrastructure.Models;
+
+namespace Vnr.Storage.API.Features.DecryptData.Commands
+{
+    public class DecryptMultipleFileCommandHandler : IRequestHandler<DecryptMultipleFileCommand, FileContentResultModel>
+    {
+        public Task<FileContentResultModel> Handle(DecryptMultipleFileCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
