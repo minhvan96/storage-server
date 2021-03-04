d0133aeadb1cd025299675d7e022475282af907e
van.nguyen <minhvan.996@outlook.com>
2021-03-04 11:28:24 +0700

    add stream multiple file command, update upload file with and without encrypt

--- Vnr.Storage/Vnr.Storage.API/Features/StreamedUploadPhysical/Commands/StreamMultipleFileUploadCommand.cs
+++ Vnr.Storage/Vnr.Storage.API/Features/StreamedUploadPhysical/Commands/StreamMultipleFileUploadCommand.cs
@@ -2,17 +2,16 @@
 using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
 using Vnr.Storage.API.Infrastructure.BaseResponse;
-using Vnr.Storage.API.Infrastructure.Enums;
 
 namespace Vnr.Storage.API.Features.StreamedUploadPhysical.Commands
 {
-    public class StreamedSingleFileUploadPhysicalCommand : IRequest<ResponseModel>
+    public class StreamMultipleFileUploadCommand : IRequest<ResponseModel>
     {
         [Required]
-        public IFormFile File { get; set; }
+        public IFormFileCollection Files { get; set; }
 
         [Required]
-        public Archive Archive { get; set; }
+        public string Archive { get; set; }
 
         public bool Encrypt { get; set; }
     }
---
+++ Vnr.Storage/Vnr.Storage.API/Features/StreamedUploadPhysical/Commands/StreamMultipleFileUploadCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Vnr.Storage.API.Configuration;
+using Vnr.Storage.API.Infrastructure.BaseResponse;
+using Vnr.Storage.API.Infrastructure.Data;
+
+namespace Vnr.Storage.API.Features.StreamedUploadPhysical.Commands
+{
+    public class StreamMultipleFileUploadCommandHandler : IRequestHandler<StreamMultipleFileUploadCommand, ResponseModel>
+    {
+        private readonly IHttpContextAccessor _accessor;
+        private readonly long _streamFileLimitSize;
+        private readonly string[] _permittedExtensions;
+        private readonly string _contentRootPath;
+        private static readonly FormOptions _defaultFormOptions = new FormOptions();
+        private readonly StorageContext _context;
+
+        public StreamMultipleFileUploadCommandHandler(IConfiguration configuration, IWebHostEnvironment env, IHttpContextAccessor accessor, StorageContext context)
+        {
+            var fileSizeLimitConfiguration = configuration.GetSection(nameof(FileSizeLimitConfiguration)).Get<FileSizeLimitConfiguration>();
+            _streamFileLimitSize = fileSizeLimitConfiguration.StreamFileSizeLimit;
+            var streamedFileUploadPhysicalPermittedExtensionsConfiguration = configuration
+               .GetSection(nameof(StreamedFileUploadPhysicalPermittedExtensionsConfiguration))
+               .Get<StreamedFileUploadPhysicalPermittedExtensionsConfiguration>();
+            _permittedExtensions = streamedFileUploadPhysicalPermittedExtensionsConfiguration.SingleFileUploadPermittedExtensions;
+            _contentRootPath = env.ContentRootPath;
+            _accessor = accessor;
+            _context = context;
+        }
+
+        public Task<ResponseModel> Handle(StreamMultipleFileUploadCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
