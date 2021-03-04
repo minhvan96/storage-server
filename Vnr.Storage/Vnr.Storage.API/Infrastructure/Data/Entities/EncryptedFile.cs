dfde48922b1edf193e6fb30cbf938b0cd504c795
van.nguyen <minhvan.996@outlook.com>
2021-03-04 10:41:45 +0700

    update entity

--- Vnr.Storage/Vnr.Storage.API/Infrastructure/Data/Entities/EncryptedFile.cs
+++ Vnr.Storage/Vnr.Storage.API/Infrastructure/Data/Entities/EncryptedFile.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vnr.Storage.API.Infrastructure.Data.Entities
 {
     public class EncryptedFile
     {
         public Guid Id { get; set; }
+
+        [MaxLength(255)]
         public string FileName { get; set; }
+
+        [MaxLength(512)]
+        public string Path { get; set; }
+
+        [MaxLength(512)]
         public string FullPath { get; set; }
     }
 }
--- Vnr.Storage/Vnr.Storage.API/Infrastructure/Data/Entities/RijndaelKey.cs
+++ Vnr.Storage/Vnr.Storage.API/Infrastructure/Data/Entities/RijndaelKey.cs
@@ -1,10 +1,15 @@
-﻿namespace Vnr.Storage.API.Infrastructure.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vnr.Storage.API.Infrastructure.Data.Entities
 {
     public class RijndaelKey
     {
         public long Id { get; set; }
 
+        [MaxLength(256)]
         public string Key { get; set; }
+
+        [MaxLength(256)]
         public string IV { get; set; }
     }
 }
