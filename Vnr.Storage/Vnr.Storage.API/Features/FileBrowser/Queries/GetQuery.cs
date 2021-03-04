760bd9c316508117267091e3c8f5bf2c9ff38fdd
van.nguyen <minhvan.996@outlook.com>
2021-03-04 11:31:39 +0700

    add Get File by ID

---
+++ Vnr.Storage/Vnr.Storage.API/Features/FileBrowser/Queries/GetQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using Vnr.Storage.API.Infrastructure.BaseResponse;
+using Vnr.Storage.API.Infrastructure.Data.Entities;
+
+namespace Vnr.Storage.API.Features.FileBrowser.Queries
+{
+    public class GetQuery : IRequest<ResponseModel<EncryptedFile>>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Vnr.Storage/Vnr.Storage.API/Features/FileBrowser/Queries/GetQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using Vnr.Storage.API.Infrastructure.BaseResponse;
+using Vnr.Storage.API.Infrastructure.Data.Entities;
+
+namespace Vnr.Storage.API.Features.FileBrowser.Queries
+{
+    public class GetQueryHandler : IRequestHandler<GetQuery, ResponseModel<EncryptedFile>>
+    {
+        public Task<ResponseModel<EncryptedFile>> Handle(GetQuery request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
