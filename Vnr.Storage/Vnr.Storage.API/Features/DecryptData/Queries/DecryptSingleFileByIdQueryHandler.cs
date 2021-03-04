9ad74bee4756e37e75b57714f6704f4dba7052e1
van.nguyen <minhvan.996@outlook.com>
2021-03-04 11:12:26 +0700

    update loadfile From Archive by ID

--- Vnr.Storage/Vnr.Storage.API/Features/DecryptData/Queries/DecryptSingleFileByIdQueryHandler.cs
+++ Vnr.Storage/Vnr.Storage.API/Features/DecryptData/Queries/DecryptSingleFileByIdQueryHandler.cs
@@ -1,9 +1,15 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
+using Vnr.Storage.API.Infrastructure.Crypto.RijndaelCrypto;
 using Vnr.Storage.API.Infrastructure.Data;
 using Vnr.Storage.API.Infrastructure.Models;
 
@@ -14,16 +20,43 @@ namespace Vnr.Storage.API.Features.DecryptData.Queries
         private readonly IHttpContextAccessor _accessor;
         private static readonly FormOptions _defaultFormOptions = new FormOptions();
         private readonly StorageContext _context;
+        private readonly string _contentRootPath;
 
-        public DecryptSingleFileByIdQueryHandler(IHttpContextAccessor accessor, StorageContext context)
+        public DecryptSingleFileByIdQueryHandler(IWebHostEnvironment env, IHttpContextAccessor accessor, StorageContext context)
         {
             _accessor = accessor;
             _context = context;
+            _contentRootPath = env.ContentRootPath;
         }
 
-        public Task<FileContentResultModel> Handle(DecryptSingleFileByIdQuery request, CancellationToken cancellationToken)
+        public async Task<FileContentResultModel> Handle(DecryptSingleFileByIdQuery request, CancellationToken cancellationToken)
         {
+            var encryptedFile = await _context.EncryptedFiles
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            var encryptedFileAbsolutePath = Path.Combine(_contentRootPath, encryptedFile.Path);
+
+            using (FileStream fs = File.Open(encryptedFileAbsolutePath, FileMode.Open))
+            {
+                byte[] data = new BinaryReader(fs).ReadBytes((int)fs.Length);
+                string result = System.Text.Encoding.UTF8.GetString(data);
+                Console.WriteLine(result);
+            }
+
             throw new NotImplementedException();
+        }
+
+        private async Task<byte[]> DecryptFileContent(byte[] content)
+        {
+            RijndaelManaged myRijndael = new RijndaelManaged();
+
+            var rijndaeData = await _context.RijndaelKeys.FirstOrDefaultAsync();
+            myRijndael.Key = Convert.FromBase64String(rijndaeData.Key);
+            myRijndael.IV = Convert.FromBase64String(rijndaeData.IV);
+
+            var decryptedFileContent = RijndaelCrypto.DecryptDataFromBytes(content, myRijndael.Key, myRijndael.IV);
+            return decryptedFileContent;
         }
     }
 }
