8e87a7e8e66e2e3337eb589e919b950e831c78b9
van.nguyen <minhvan.996@outlook.com>
2021-03-05 14:41:41 +0700

    refactor

--- Vnr.Storage/Vnr.Storage.Security/Crypto/RijndaelCrypto/RijndaelHelper.cs
+++ Vnr.Storage/Vnr.Storage.Security/Crypto/RijndaelCrypto/RijndaelHelper.cs
@@ -7,21 +7,21 @@ namespace Vnr.Storage.Security.Crypto.RijndaelCrypto
         public static void CanPerformEncrypt(byte[] data, byte[] key, byte[] iv)
         {
             if (data == null || data.Length <= 0)
-                throw new ArgumentNullException("Empty Data");
+                throw new ArgumentNullException("data");
             if (key == null || key.Length <= 0)
-                throw new ArgumentNullException("Key");
+                throw new ArgumentNullException("key");
             if (iv == null || iv.Length <= 0)
-                throw new ArgumentNullException("IV");
+                throw new ArgumentNullException("iv");
         }
 
         public static void CanPerformDecrypt(byte[] data, byte[] key, byte[] iv)
         {
             if (data == null || data.Length <= 0)
-                throw new ArgumentNullException("cipherText");
+                throw new ArgumentNullException("data");
             if (key == null || key.Length <= 0)
-                throw new ArgumentNullException("Key");
+                throw new ArgumentNullException("key");
             if (iv == null || iv.Length <= 0)
-                throw new ArgumentNullException("IV");
+                throw new ArgumentNullException("iv");
         }
     }
 }
